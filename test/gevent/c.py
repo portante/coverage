bcc083bf3fc83de89d7e6eb82e923719a9c299b9
Peter Portante <peter.portante@redhat.com>
2013-03-17 18:06:31 -0400

    Add a sample program for gevent
    Signed-off-by: Peter Portante <peter.portante@redhat.com>

---
+++ test/gevent/c.py
@@ -0,0 +1,38 @@
+from gevent import monkey
+monkey.patch_thread()
+import threading
+import gevent.queue as Queue
+
+class Producer(threading.Thread):
+    def __init__(self, q):
+        threading.Thread.__init__(self)
+        self.q = q
+
+    def run(self):
+        for i in range(10):
+            self.q.put(i)
+        self.q.put(None)
+
+class Consumer(threading.Thread):
+    def __init__(self, q):
+        threading.Thread.__init__(self)
+        self.q = q
+
+    def run(self):
+        while True:
+            i = self.q.get()
+            if i is None:
+                return
+            print i
+
+def main():
+    q = Queue.Queue()
+    p = Producer(q)
+    c = Consumer(q)
+    c.start()
+    p.start()
+    p.join()
+    c.join()
+
+if __name__ == "__main__":
+    main()
